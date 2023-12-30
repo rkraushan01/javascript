fe40ebc6fe3539c0f75911d149eba90015798dc3
rkraushan01 <109223945+rkraushan01@users.noreply.github.com>
2023-12-30 12:10:38 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3);//we are using nodejs, not browser
+console.log(3+3);
+console.log("Raushan");
+// let a;//indefined, value not defined  
+// null=>standalone value
+console.log(typeof(null));
+console.log(typeof(undefined));
+//symbol=>unique
+
+
+//objectf
