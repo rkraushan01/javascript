6b6c51cf7008a46ed0cc73b841405fe4c2e6aa4b
rkraushan01 <109223945+rkraushan01@users.noreply.github.com>
2024-01-03 14:38:23 +0000

    dataTpes done

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -1,12 +1,30 @@
-"use strict";//treat all JS code as newer version
-//alert(3+3);//we are using nodejs, not browser
-console.log(3+3);
-console.log("Raushan");
-// let a;//indefined, value not defined  
-// null=>standalone value
-console.log(typeof(null));
-console.log(typeof(undefined));
-//symbol=>unique
+// "use strict";//treat all JS code as newer version
+// //alert(3+3);//we are using nodejs, not browser
+// console.log(3+3);
+// console.log("Raushan");
+// // let a;//indefined, value not defined  
+// // null=>standalone value
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+// //symbol=>unique
 
 
-//objectf
+// //objectf
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id==anotherId);
+//array
+// const arr = ["Twitter", "FB", "Instagram"];
+// console.log(typeof arr);
+
+// let myObj = {
+//     name:"Rkkkk",
+//     roll:444,
+// }
+// console.log(typeof myObj);
+const myFunction = function(a, b){
+    console.log(a*b);
+}
+myFunction(3, 2);
+console.log(typeof(myFunction));
