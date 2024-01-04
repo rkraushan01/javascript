39a1e11cb83c2abe96a752f384167c8e2ebf27e9
rkraushan01 <109223945+rkraushan01@users.noreply.github.com>
2024-01-04 17:33:39 +0000

    arrays config done

---
+++ basics2/array.js
@@ -0,0 +1,45 @@
+// const arr = [1, 2, 3, 4, 5];
+// const arr2 = ["A", 46, true];
+// const arr3 = new Array(1, 2, 333);
+// console.log(arr);
+// console.log(arr2);
+// console.log(arr3);
+
+// console.log(typeof arr);
+// console.log(typeof arr2);
+// console.log(typeof arr3);
+
+// console.log(arr.includes(88));
+// console.log(arr.indexOf(88));
+// const arr2 = arr.slice(0, 2);
+// console.log(arr2);
+// const arr3 = arr.splice(0, 2);
+// console.log(arr3);
+// console.log(arr);
+// arr.forEach((item, index) =>{
+//     console.log(`${index}th index: ${item}`);
+// });
+
+// const arr1 = [ "a", "b"];
+// const arr2 = [ "c", "d"];
+// // arr1.push(arr2)
+// const arr3 = arr1.concat(arr2);
+// const arr4 = [...arr1, ...arr2]
+
+// console.log(arr3);
+// console.log(arr4);
+
+// const arr5 = [1, [2, [3, [4]]]];
+// const arr6 = arr5.flat(Infinity);
+// console.log(arr5);
+// console.log(arr6);
+
+// console.log(Array.isArray("Ritesh"));
+// console.log(Array.from("Ritesh"));
+// //interesting
+// //confused between keys and values
+// console.log(Array.from({name:"hitessh"}))
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(Array.of(a, b,c));
