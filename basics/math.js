a8e2517d67b8eab59bf76f47ec044be10e34dce0
rkraushan01 <109223945+rkraushan01@users.noreply.github.com>
2024-01-04 09:46:31 +0000

    maths topic done

---
+++ basics/math.js
@@ -0,0 +1,17 @@
+// const balance = 123.956;
+// console.log(balance.toPrecision(3));
+
+//////////Maths////////////// 
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(8.3));
+// console.log(Math.round(8.9));
+// console.log(Math.sqrt(16));
+console.log(Math.random());//gives values between 0 and 1
+
+//to get values greater than 1, we multiply it by 10
+console.log((Math.floor(Math.random()*10))+1);
+const max = 20;
+const min = 10;
+console.log((Math.floor(Math.random()*(max-min+1)))+min);
+
